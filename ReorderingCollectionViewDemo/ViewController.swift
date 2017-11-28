fecd50aaa9762e52bf0f810e9ed7b5e8fd253bcb
Oleksii Karzov <alex@Alexs-MacBook-Pro.local>
2017-11-28 09:52:10 +0200

    Initial Commit

---
+++ ReorderingCollectionViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReorderingCollectionViewDemo
+//
+//  Created by Oleksii Karzov on 11/28/17.
+//  Copyright © 2017 olekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
